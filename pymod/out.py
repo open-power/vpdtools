41e5dcaed0eb268efca567faf937eb2cd8760b80
Jason Albert <albertj@us.ibm.com>
2015-03-16 15:02:11 -0500

    Adding the out pymod that was missed from earlier checkins

---
+++ pymod/out.py
@@ -0,0 +1,37 @@
+# Jason Albert - created 03/06/2014
+# Python module to define common output functions
+
+############################################################
+# Imports - Imports - Imports - Imports - Imports - Imports
+############################################################
+import os
+import cmdline
+
+############################################################
+# Variables - Variables - Variables - Variables - Variables
+############################################################
+class VarBox:
+    pass
+
+__m = VarBox()
+__m.indent = 0
+
+############################################################
+# Function - Functions - Functions - Functions - Functions
+############################################################
+# Common function for error printing
+def error(message):
+    print((' ' * __m.indent) + ("ERROR: %s" % message))
+
+# Common function for debug printing
+def debug(message):
+    print((' ' * __m.indent) + ("DEBUG: %s" % message))
+
+def msg(message):
+    print((' ' * __m.indent) + message)
+
+def setIndent(num):
+    """ 
+    Sets the output indent on all printed lines
+    """
+    __m.indent = num
