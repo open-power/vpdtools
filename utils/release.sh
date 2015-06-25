887f417730f1f7486e3b998dfb70a4cfe1ebc07a
Jason Albert <albertj@us.ibm.com>
2015-06-25 14:39:57 -0500

    Updated release.sh to allow user to pick release dir

--- utils/release.sh
+++ utils/release.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
+# Simple script to copy the tool to a "release" dir
+# Useful if users don't want to get it from git themselves
+
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if [ -z "$1" ];  then
+    echo "Release path is a required arg"
+    exit 1
+fi
+
+if [ ! -d "$1" ]; then
+    echo "$1 doesn't exist, please create and re-run"
+    exit 1
+fi
 
 # Copy the scripts out the release point
-cp ../createVpd.py /gsa/rchgsa/projects/o/optk/vpdtemplate/.
-cp ../reverseVpd.py /gsa/rchgsa/projects/o/optk/vpdtemplate/.
-chmod +x /gsa/rchgsa/projects/o/optk/vpdtemplate/createVpd.py
-chmod +x /gsa/rchgsa/projects/o/optk/vpdtemplate/reverseVpd.py
+cp $SCRIPTDIR/../createVpd.py $1/.
+cp $SCRIPTDIR/../reverseVpd.py $1/.
+chmod +x $1/createVpd.py
+chmod +x $1/reverseVpd.py
 
 # Copy out the pymods
-cp -r ../pymod /gsa/rchgsa/projects/o/optk/vpdtemplate/.
-chmod +r /gsa/rchgsa/projects/o/optk/vpdtemplate/pymod
+cp -r $SCRIPTDIR/../pymod $1/.
+chmod +r $1/pymod
 
 # Copy the examples out
-for test in `ls ../tests/pass/`;
+mkdir $1/examples
+for test in `ls $SCRIPTDIR/../tests/pass/`;
 do
-  cp -r ../tests/pass/$test /gsa/rchgsa/projects/o/optk/vpdtemplate/examples/.
-  chmod +x /gsa/rchgsa/projects/o/optk/vpdtemplate/examples/$test
-  chmod -R +r /gsa/rchgsa/projects/o/optk/vpdtemplate/examples/$test
+  cp -r $SCRIPTDIR/../tests/pass/$test $1/examples/.
+  chmod +x $1/examples/$test
+  chmod -R +r $1/examples/$test
 done
